45525965287a149bdfa19c0faa3d778d9a01eafb
Dave Green <david.green@tookitaway.co.uk>
2013-07-08 21:46:54 +0100

    Update verbose and progress information.

--- PowerShell/exch2003mailboxlastlogon.ps1
+++ PowerShell/exch2003mailboxlastlogon.ps1
@@ -11,12 +11,12 @@ param(
 
 # Get the WMI objects from both Exchange and AD
 # Write progress to the console, to keep the user updated.
-Write-Verbose "Getting data from WMI (root\MicrosoftExchangeV2\Exchange_mailbox)"
-Write-Progress -Activity "Preparing Run" -Status "Getting data from WMI (root\MicrosoftExchangeV2\Exchange_mailbox)" -PercentComplete 0
+Write-Verbose "Getting data from $ExchServerName WMI (root\MicrosoftExchangeV2\Exchange_mailbox)"
+Write-Progress -Activity "Preparing Run" -Status "Getting data from $ExchServerName WMI (root\MicrosoftExchangeV2\Exchange_mailbox)" -PercentComplete 0
 $exchusers = Get-WmiObject -ComputerName $ExchServerName -Namespace root\MicrosoftExchangeV2 -Class Exchange_mailbox | Select-Object LegacyDN, MailboxDisplayName, Size, TotalItems
-Write-Progress -Activity "Preparing Run" -Status "Getting data from WMI (root\directory\ldap\ds_user)" -PercentComplete 50
 Write-Verbose "Got Exchange WMI data."
-Write-Verbose "Getting data from WMI (root\directory\ldap\ds_user)"
+Write-Progress -Activity "Preparing Run" -Status "Getting data from $ADServerName WMI (root\directory\ldap\ds_user)" -PercentComplete 50
+Write-Verbose "Getting data from $ADServerName WMI (root\directory\ldap\ds_user)"
 $adusers = Get-WmiObject -ComputerName $ADServerName -Namespace root\directory\ldap -Class ds_user | Select-Object DS_legacyExchangeDN, DS_proxyAddresses, DS_mail, DS_userAccountControl, DS_extensionAttribute1, DS_CN, DS_LastLogon, DS_accountExpires
 Write-Progress -Activity "Preparing Run" -Status "Got WMI Data." -PercentComplete 100
 Write-Verbose "Got AD WMI data."
