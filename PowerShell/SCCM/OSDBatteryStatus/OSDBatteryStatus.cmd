5f82262ff22299804660131b60bdb2c4f7f267a2
Dave Green <david.green@tookitaway.co.uk>
2014-07-31 23:16:45 +0100

    Add OSDBatteryStatus script

---
+++ PowerShell/SCCM/OSDBatteryStatus/OSDBatteryStatus.cmd
@@ -0,0 +1,4 @@
+set _sui=ServiceUI.exe
+if /I "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" set _sui=ServiceUI_x64.exe
+
+"%_sui%" -process:TSProgressUI.exe %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File OSDBatteryStatus.ps1
