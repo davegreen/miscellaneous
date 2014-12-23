d4ee10bdfb9b4964a9d531236695be553751a98f
Dave Green <david.green@tookitaway.co.uk>
2014-12-23 12:20:02 +0000

    Add comments\metadata.

--- PowerShell/Import-AXUser.ps1
+++ PowerShell/Import-AXUser.ps1
@@ -1,3 +1,35 @@
+<# 
+    .Synopsis
+    Uses AX PowerShell cmdlets to Import AX users from a CSV file.
+
+    .Description
+    Uses AX PowerShell cmdlets to Import AX users from a CSV file and works with the corresponding Export-AXUser script.
+
+    .Parameter CsvFile
+    The path to the CSV file containing the users to import.
+
+    .Parameter DefaultCompany
+    Do not specify the company to add users to (use default).
+
+    .Parameter NoClobber
+    Do not override security for existing users.
+
+    .Example
+    .\Import-AXUser.ps1 -CsvFile .\Users.csv
+    Import users from the CSV file in the current directory.
+        
+    .Notes
+    Name  : Import-AXUser.ps1
+    Author: David Green
+  
+    .Link
+    http://www.tookitaway.co.uk
+    https://github.com/davegreen/miscellaneous.git
+#>
+
+# The Setup-Management and Import-AXModule portaions of theis script are from a script originally by Vishy (http://vgrandhi.wordpress.com).
+# (http://vgrandhi.wordpress.com/2014/04/22/ax-2012-powershell-script-to-export-users-and-roles/)
+
 [CmdletBinding()]
 Param
 (
