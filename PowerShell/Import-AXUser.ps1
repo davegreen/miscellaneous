8eebbc6d9a1ede56e7436338ff09bbbcddb7adf6
Dave Green <david.green@tookitaway.co.uk>
2014-12-23 11:49:29 +0000

    Create Import-AXUser.ps1
    Import AX users created with the Export-AXUser.ps1 script.

---
+++ PowerShell/Import-AXUser.ps1
@@ -0,0 +1,143 @@
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$true,
+    ParameterSetName='CSV')]
+    [string]$CsvFile = "Users.csv",
+    [switch]$EnableAll,
+    [switch]$DefaultCompany,
+    [switch]$NoClobber
+)
+
+Function Import-AXModule($axModuleName, $disableNameChecking, $isFile)
+{
+    Try
+    {
+        $outputmessage = "Importing " + $axModuleName
+        Write-Verbose $outputmessage
+ 
+        if($isFile)
+        {
+            $dynamicsSetupRegKey = Get-Item "HKLM:\SOFTWARE\Microsoft\Dynamics\6.0\Setup"
+            $sourceDir = $dynamicsSetupRegKey.GetValue("InstallDir")
+            $axModuleName = "ManagementUtilities\" + $axModuleName + ".dll"
+            $axModuleName = Join-Path $sourceDir $axModuleName
+        }
+
+        if($disableNameChecking)
+        {
+            Import-Module $axModuleName -DisableNameChecking
+        }
+
+        else
+        {
+            Import-Module $axModuleName
+        }
+    }
+
+    Catch
+    {
+        $outputmessage = "Could not load file " + $axModuleName
+        Write-Error $outputmessage
+    }
+}
+
+Function Setup-Management()
+{ 
+    $dynamicsSetupRegKey = Get-Item "HKLM:\SOFTWARE\Microsoft\Dynamics\6.0\Setup"
+    $sourceDir = $dynamicsSetupRegKey.GetValue("InstallDir")
+    $dynamicsAXModulesPath = join-path $sourceDir "ManagementUtilities\Modules"
+    $env:PSModulePath = $env:PSModulePath + ";" + $dynamicsAXModulesPath
+
+    Import-AXModule "AxUtilLib" $false $true
+    Import-AXModule "AxUtilLib.PowerShell" $true $false
+    Import-AXModule "Microsoft.Dynamics.Administration" $false $false
+    Import-AXModule "Microsoft.Dynamics.AX.Framework.Management" $false $false
+}
+
+Function Set-AxSecurity()
+{
+    Write-Verbose "Setting security roles for $($user.UserName) ($($user.Name))."
+    $securityRoles = Get-AXSecurityRole -AxUserID $AXUserId
+    $userRoles = @()
+        
+    foreach ($role in $user.Role.split(","))
+    {
+        $obj = New-Object PSObject
+        $obj | Add-Member NoteProperty -Name "AOTName" -Value $role
+        $userRoles += $obj
+    }
+     
+    switch (Compare-Object -ReferenceObject $securityRoles -DifferenceObject $userRoles -property AOTName)
+    {
+        {$_.SideIndicator -eq "=>"}
+        {
+            foreach ($aot in $_.AOTName)
+            {
+                Add-AXSecurityRoleMember -AXUserId $AXUserId -AOTName $aot
+                Write-Verbose "Added $($user.UserName) to the role $($aot)."
+            }
+        }
+        
+        {$_.SideIndicator -eq "<="}
+        {
+            #Nothing Native in PowerShell to remove AX security roles from users.
+            Write-Verbose "$($user.UserName) has the role $($_.AOTName), but this is not specified in the import data."
+        }
+
+        {$_.SideIndicator -eq "=="}
+        {
+            Write-Verbose "$($user.UserName) already has the role $($_.AOTName)."
+        }
+    }
+}
+
+Setup-Management
+
+$Users = Import-Csv $CsvFile
+$UserDomain = $env:userdnsdomain
+$AXUsers = Get-AXUser | where {$_.AXUserId -and $_.UserName}
+$AXUserId = $null
+
+foreach ($user in $Users)
+{
+    if ($user.UserName.length -gt 8)
+    {
+        $AXUserId = ($user.UserName.SubString(0,7) + $user.UserName.substring($user.UserName.length -1))
+        Write-Verbose "Altered AXUserId from $($user.UserName) to $AXUserId in order to fit length requirements."
+    }
+
+    else
+    {
+        $AXUserId = $user.UserName
+    }
+
+    Write-Verbose "Checking if user $($user.UserName) ($($user.Name)) exists."
+
+    if ($AXUsers.UserName -notcontains $user.UserName)
+    {
+        if ($DefaultCompany)
+        {
+            Write-Verbose "Creating User $($user.UserName) ($($user.Name))."
+            New-AXUser -AccountType $user.AccountType -AXUserId $AXUserId -UserDomain $UserDomain -UserName $user.UserName
+        }
+
+        else
+        {
+            Write-Verbose "Creating User $($user.UserName) ($($user.Name)) in company $($user.Company)."
+            New-AXUser -AccountType $user.AccountType -AXUserId $AXUserId -UserDomain $UserDomain -UserName $user.UserName -Company $user.Company
+        }
+
+        Set-AxSecurity
+    }
+
+    else
+    {
+        Write-Verbose "User $($user.UserName) ($($user.Name)) already exists."
+    }
+
+    if (!$NoClobber)
+    {
+        Set-AxSecurity
+    }
+}
